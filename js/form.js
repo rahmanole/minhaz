9cf7f28cf7c6f00b93aedf93e1e591b7a20fef85
Minhazur Rahman <minhazduphy@gmail.com>
2018-09-19 17:52:03 +0600

    add

--- js/form.js
+++ js/form.js
@@ -1,11 +1,16 @@
+
 function formValidation(){
 	var pass1 = document.getElementById("pass1").value;
 	var pass2 = document.getElementById("pass2").value;
-	if(pass1==pass2){
-		document.getElementById("pass").innerHTML ="<h3>Password matched!</h3>";
-
+		
+	if(pass1==="" && pass2===""){
+		document.getElementById('pass').innerHTML ="<h3>Enetr password!</h3>";
 	}else{
-		document.getElementById("pass").innerHTML=pass1;
+		if(pass1==pass2){
+		document.getElementById('pass').innerHTML ="<h3>Password matched!</h3>";
+		}else{
+			document.getElementById('pass').innerHTML="<h3>Password does not matched!</h3>";
+		}
 	}
-	
+
 }
