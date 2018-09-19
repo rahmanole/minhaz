49664d910e91cb407d91757a83957f60d55c1c30
Minhazur Rahman <minhazduphy@gmail.com>
2018-09-19 14:54:20 +0600

    new

--- js/form.js
+++ js/form.js
@@ -1,4 +1,11 @@
 function formValidation(){
-	var pass1 = document.getElementById("fname").value;
-	document.write(pass1);
+	var pass1 = document.getElementById("pass1").value;
+	var pass2 = document.getElementById("pass2").value;
+	if(pass1==pass2){
+		document.getElementById("pass").innerHTML ="<h3>Password matched!</h3>";
+
+	}else{
+		document.getElementById("pass").innerHTML=pass1;
+	}
+	
 }
